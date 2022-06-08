2f3d44c11a84de544f8bc241b61cb9c46a9056e2
Saoa35 <saoamah@gmail.com>
2022-06-08 22:02:53 +0200

    import usePageData

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,12 @@
+import usePageData from '../../custom-hooks/usePageData';
 import Banner from '../Banner/Banner';
 import Header from '../Header/Header';
 import './App.scss';
 
 function App() {
+
+  const data = usePageData('foodBand');
+
   return (
     <div className="App">
       
