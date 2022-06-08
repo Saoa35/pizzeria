b82b838602716753892e97dbccde76c6d15db7be
Saoa35 <saoamah@gmail.com>
2022-06-08 19:13:34 +0200

    add banner  & header comp.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,19 @@
+import Banner from '../Banner/Banner';
+import Header from '../Header/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       
+      <Header/>
+
+      <main className='main'>
+        <div className='container'>
+          <Banner/>
+        </div>
+      </main>
+
     </div>
   );
 }
