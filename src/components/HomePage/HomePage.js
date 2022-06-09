422f5109788b299d07bcc1d6a03c1f1c8c43e096
Saoa35 <saoamah@gmail.com>
2022-06-09 19:25:16 +0200

    create HomePage component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Restaurants from '../Restaurants/Restaurants';
+
+
+const HomePage = () => {
+
+    return (
+        <>
+            <Banner/>
+            <Restaurants/>
+        </>
+    )
+}
+
+export default HomePage;
