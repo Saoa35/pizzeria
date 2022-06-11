aa2d2f307c90833709031f9998f1606c008b6587
Saoa35 <saoamah@gmail.com>
2022-06-11 22:12:56 +0200

    add props in productcard

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({ id, description, image, name, price }) => {
+
+    const src = require(`../../assets/${image}`);
 
     return(
         <div className='card'>
-            <img src='${image}' alt='image' className='card-image' />
+            <img src={src} alt={src} className='card-image' />
         <div className='card-text'>
 
             <div className='card-heading'>
-                <h3 className='card-title card-title-reg'>${'name'}</h3>
+                <h3 className='card-title card-title-reg'>{name}</h3>
             </div>
 
             <div className='card-info'>
-                <div className='ingredients'>${'description'}</div>
+                <div className='ingredients'>{description}</div>
             </div>
 
             <div className='card-buttons'>
-                <button className='button button-primary button-add-cart' id='${id}'>
+                <button className='button button-primary button-add-cart'>
                     <span className='button-card-text'>В корзину</span>
                     <span className='button-cart-svg'></span>
                 </button>
-                <strong className='card-price-bold card-price'>$${'price'}</strong>
+                <strong className='card-price-bold card-price'>${price}</strong>
             </div>
         </div>
         </div>
