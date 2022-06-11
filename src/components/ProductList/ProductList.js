2c4038940babf78bbcb3c9c241a10422aa24ab8e
Saoa35 <saoamah@gmail.com>
2022-06-11 21:08:52 +0200

    +useParams

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductList = () => {
+
+    const  { products } = useParams();
 
     return(
         <section className="menu">
