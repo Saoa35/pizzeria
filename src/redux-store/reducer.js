582877a9e0b56238ff5ec1cbba4a01e9a423cb0b
Saoa35 <saoamah@gmail.com>
2022-06-12 16:38:44 +0200

    create & set localstorage

--- src/redux-store/reducer.js
+++ src/redux-store/reducer.js
@@ -1,9 +1,17 @@
+const STORE_USER = 'store_user';
 
+const initialState = localStorage.getItem(STORE_USER);
+const initialStateToObject = JSON.parse(initialState);
 
-const reducer = (state = null, action) => {
+const currentState = {
+    login: initialStateToObject ? initialStateToObject.login : null
+}
+
+const reducer = (state = currentState, action) => {
 
     switch(action.type) {
         case 'LOG_IN': {
+            localStorage.setItem(STORE_USER, JSON.stringify(action.payload));
             return state = action.payload;
         }
         default:
