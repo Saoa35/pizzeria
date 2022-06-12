c3f108f58d989f3a93a29e1a590b48744a681c5f
Saoa35 <saoamah@gmail.com>
2022-06-12 15:59:19 +0200

    + login to buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import ModalAuth from "../ModalAuth/ModalAuth";
 
-const Header = () => {
+const Header = ({ login }) => {
+
+  const [isLoginModal, setIsLoginModal] = useState(false);
+
+  const handleLoginModal = () => {
+    setIsLoginModal(!isLoginModal);
+  }
+
+    console.log(login);
 
     const logo = require('../../assets/img/icon/logo.svg').default;
 
@@ -21,25 +31,33 @@ const Header = () => {
               <div className="store-header__buttons">
                 <span className="store-header__user-name"></span>
     
-                <button className="button button-primary button-auth">
+                <button className="button button-primary button-auth" onClick={handleLoginModal}>
                   <span className="button-auth-svg"></span>
                   <span className="button-text">Войти</span>
                 </button>
+
                 <button className="button button-cart" id="cart-button">
                   <span className="button-cart-svg"></span>
                   <span className="button-text">Корзина</span>
                 </button>
-                <button className="button button-primary button-out">
+                
+                {login && <button className="button button-primary button-out">
                   <span className="button-text">Выйти</span>
                   <span className="button-out-svg"></span>
-                </button>
+                </button>}
     
               </div>
     
             </div>
           </div>
+
+          {isLoginModal && <ModalAuth onClose={handleLoginModal}/>}
         </header>
       )
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  login: state
+})
+
+export default connect(mapStateToProps) (Header);
