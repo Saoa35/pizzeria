5159ee1b941cce3844a2f4225cafd8ab48d58e8a
Saoa35 <saoamah@gmail.com>
2022-06-14 19:53:31 +0200

    create modalcart

--- src/components/ModalCart/ModalCart.js
+++ src/components/ModalCart/ModalCart.js
@@ -5,11 +5,38 @@ const ModalCart = () => {
 
     return(
         <Portal>
-            <div class='modal modal-cart'>
-                <div class='modal-dialog'>
-                    <div class='modal-header'>
-                        <h3 class='modal-title'>Корзина</h3>
-                        <button id='modal-cart-close-btn' class='close'>&times;</button>
+            <div className='modal modal-cart'>
+                <div className='modal-dialog'>
+                    <div className='modal-header'>
+                        <h3 className='modal-title'>Корзина</h3>
+                        <button id='modal-cart-close-btn' className='close'>&times;</button>
+                    </div>
+                    <div className='modal-body'>
+                        <div className='food-row'>
+                            <span className='food-name'>Ролл Угорь стандарт</span>
+                            <strong className='food-price'>250$</strong>
+                            <div className='food-counter'>
+                                <button className='counter-button'>-</button>
+                                <span className='counter'>1</span>
+                                <button className='counter-button'>+</button>
+                            </div>
+                        </div>
+                        <div className='food-row'>
+                            <span className='food-name'>Ролл Угорь стандарт</span>
+                            <strong className='food-price'>250$</strong>
+                            <div className='food-counter'>
+                                <button className='counter-button'>-</button>
+                                <span className='counter'>1</span>
+                                <button className='counter-button'>+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='modal-footer'>
+                        <span className='modal-pricetag'>1250$</span>
+                        <div className='footer-buttons'>
+                            <button className='button button-primary'>Оформить заказ</button>
+                            <button className='button clear-cart'>Отмена</button>
+                        </div>
                     </div>
                 </div>
             </div>
