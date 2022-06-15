dc01adacc1ccb66d8678b5a1b942d2c22aa75a7e
Saoa35 <saoamah@gmail.com>
2022-06-15 20:53:04 +0200

    create handleCountProduct method

--- src/components/ModalCart/ModalCart.js
+++ src/components/ModalCart/ModalCart.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Portal from '../Portal/Portal';
 
-const ModalCart = ({ onClose }) => {
+const ModalCart = ({ onClose, login }) => {
+
+    const dataFromStorage = JSON.parse(localStorage.getItem(login));
+    const [data, setData] = useState(dataFromStorage);
+    const totalPrice = data?.reduce((prev, acc) => prev + acc.cost * acc.count, 0);
+
+    const handleCountProduct = (id, count, k) => {
+        
+        if(count === 1 && k === -1) {
+            const filteredData = data.filter(product => product.id !== id);
+            localStorage.setItem(login, JSON.stringify(filteredData));
+        } else {
+            const changedData = data.map(product => {
+                if(product.id === id) {
+                    return { ...product, count:product.count + k}
+                } else return product;
+            });
+            localStorage.setItem(login, JSON.stringify(changedData));
+        }
+        setData(JSON.parse(localStorage.getItem(login)));
+    }
 
     return(
         <Portal>
@@ -11,28 +31,28 @@ const ModalCart = ({ onClose }) => {
                         <h3 className='modal-title'>Корзина</h3>
                         <button onClick={onClose} className='close'>&times;</button>
                     </div>
-                    <div className='modal-body'>
-                        <div className='food-row'>
-                            <span className='food-name'>Ролл Угорь стандарт</span>
-                            <strong className='food-price'>250$</strong>
-                            <div className='food-counter'>
-                                <button className='counter-button'>-</button>
-                                <span className='counter'>1</span>
-                                <button className='counter-button'>+</button>
-                            </div>
-                        </div>
-                        <div className='food-row'>
-                            <span className='food-name'>Ролл Угорь стандарт</span>
-                            <strong className='food-price'>250$</strong>
-                            <div className='food-counter'>
-                                <button className='counter-button'>-</button>
-                                <span className='counter'>1</span>
-                                <button className='counter-button'>+</button>
-                            </div>
-                        </div>
-                    </div>
+                    <ul className='modal-body'>
+
+                        {data?.map(product => {
+
+                            const {id, title, cost, count} = product;
+
+                            return(
+                                <li key={id} className='food-row'>
+                                    <span className='food-name'>{title}</span>
+                                    <strong className='food-price'>${cost}</strong>
+                                    <div className='food-counter'>
+                                        <button className='counter-button' onClick={() => handleCountProduct(id, count, -1)}>-</button>
+                                        <span className='counter'>{count}</span>
+                                        <button className='counter-button' onClick={() => handleCountProduct(id, count, 1)}>+</button>
+                                    </div>
+                                </li>
+                            )
+                        })}
+
+                    </ul>
                     <div className='modal-footer'>
-                        <span className='modal-pricetag'>1250$</span>
+                        <span className='modal-pricetag'>${totalPrice}</span>
                         <div className='footer-buttons'>
                             <button className='button button-primary'>Оформить заказ</button>
                             <button className='button clear-cart'>Отмена</button>
