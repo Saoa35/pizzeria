31cfa55895893d09de0eb1ce26628796e7a22446
Saoa35 <saoamah@gmail.com>
2022-06-16 13:59:03 +0200

    replase files

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const SearchInput = () => {
-
-    return(
-        <div className="restaurants__heading">
-            <h2 className="restaurants__title">Рестораны</h2>
-            <label className="restaurants__search">
-            <input type="text" className="input input-search" placeholder="Поиск блюд и ресторанов"/>
-            </label>
-        </div>
-    )
+const SearchInput = ({ value, onChange }) => {
+  return (
+    <div className="restaurants__heading">
+      <h2 className="restaurants__title">Рестораны</h2>
+      <label className="restaurants__search">
+        <input 
+          type="text" 
+          className="input input-search" 
+          value={value}
+          onChange={onChange}
+          placeholder="Поиск блюд и ресторанов"
+        />
+      </label>
+    </div>
+  )
 }
 
 export default SearchInput;
